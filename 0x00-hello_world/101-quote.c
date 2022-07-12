70538f5fbd22068e84de8afe1bea45cc4f252fe6
nadalD <dagim.degenie@gmail.com>
2022-07-12 07:02:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,13 @@
 
 /**
  * main - prints out a string using write
- * Description: Program that prints out the string s followed by a new line                                                        
+ * Description: Program that prints out the string s followed by a new line
  * Return: int 0 when done and no errors.
- * Could not use any system calls. or any standard library functions.
- * Used write tooutput to stderror. Could have tried a method with 2>
- * the bit field is 59. I tried 58, cut off, I tried 60, 61, and higher and
- * it just won't pass because there are prob other things in buffer   
+ * Could not use any system call. Or any standard library functions.
+ * Used write tooutput to stderror. Could have tried a method with>2
+ * the bit field is 59. I tried 58, I tried 60, 61, and higher and
+ * it just won't pass because there are prob other things in buffer
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
