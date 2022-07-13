53cabfd0af651d7ba57f34ce05760620ccdae371
nadalD <dagim.degenie@gmail.com>
2022-07-13 07:57:29 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter  = 'a'; letter <= 'z'; letter++)
+	{
+		if(lettr != q && letter !=e)
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
