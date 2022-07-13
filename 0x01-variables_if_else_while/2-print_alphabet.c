93c94b5bed41c3b170c70d91b9a0105b887aaaa9
nadalD <dagim.degenie@gmail.com>
2022-07-13 07:26:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		purchar(letter);
+	putchar('\n')
+	return (0);
+}
