71a30ac23e050934fec441c6767dc882bf3bbd68
nadalD <dagim.degenie@gmail.com>
2022-07-14 06:49:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_alphanet - print alphabet lowercase
@@ -11,9 +11,8 @@ void print_alphabet(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
+	
 		_putchar(alphabet);
-	}
-
+	
 	_putchar('\n');
 }
