8409ac5353e5e29bf41d5937e603e5da3043de0a
nadalD <dagim.degenie@gmail.com>
2022-07-14 02:07:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = i + 2; k <= 9; k++)
+			for (k = j + 2; k <= 9; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
