f3a360454121e0786e63a7385145d5c0c8a0ab34
nadalD <dagim.degenie@gmail.com>
2022-07-14 05:21:03 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i + '0');
-			putchar(i + '0');
-			putchar(' ');
+			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i == 9 && j == 9)
+			if (i == 98 && j == 99)
 				continue;
 
 			putchar(',');
