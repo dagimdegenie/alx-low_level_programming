a36de1cb89a7bdaf24e5d6f85b456c0bb1cddf62
nadalD <dagim.degenie@gmail.com>
2022-07-14 04:49:55 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar(i + '0');
 			putchar(i + '0');
