3dd927ab2f0a716f39c25fff6be251e6bfc7e34b
nadalD <dagim.degenie@gmail.com>
2022-07-15 10:45:05 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_to_98 - print n to 98 counts separated by comma, followed by space and number should be printed in order
+ * print_to_98 - print all natural numbers from n to 98
  *
  * @n: input
  */
