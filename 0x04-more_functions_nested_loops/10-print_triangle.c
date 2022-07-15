6033823c3c6d8dd27d018aa54e4e62e1e135e7e7
nadalD <dagim.degenie@gmail.com>
2022-07-15 14:27:35 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,11 +15,11 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 	for (i = 0; i < size; i++)
-		{
-			for (j = size - i - 1; j > 0; j--)
-				_putchar(' ');
-			for (j = i + 1; j > 0; j--)
-				_putchar('#');
-			_putchar('\n');
-		}
+	{
+		for (j = size - i - 1; j > 0; j--)
+			_putchar(' ');
+		for (j = i + 1; j > 0; j--)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
