d99d323e2adb5f7099965dafac7e40bd48f1c0ca
nadalD <dagim.degenie@gmail.com>
2022-07-16 16:25:19 -0700

    alx-low_level_programming/0x06-pointers_
    arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	int n;
-	int a[5];
+	int k[5];
 	int *p;
 
-	a[2] = 1024;
+	k[2] = 1024;
 	p = &n;
 	/*
 	 * write your line of code here...
@@ -24,6 +24,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("k[2] = %d\n", k[2]);
 	return (0);
 }
